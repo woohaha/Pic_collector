abba5244b7447723d168d1a739f678b764eaebbf
woohaha <realwoohaha@gmail.com>
2014-03-11 14:58:59 +0800

    class cl added

--- 163.py
+++ 163.py
@@ -8,6 +8,21 @@ import glob
 import sys
 
 
+class find_cl:
+
+    def __init__(self, url):
+        self.url = url
+        self.__soup = BeautifulSoup(
+            requests.get(url).content.decode('gbk', 'ignore'))
+
+        self.article_name = self.__soup.title.h4.string
+
+        self.img_addr = [x['src'] \
+                         for x in \
+                         self.__soup.find_all('input', type="image")]
+        self.tag = re.search(r'\[.*?\]', self.article_name)
+
+
 class find_163_img:
 
     def __init__(self, url):
@@ -25,7 +40,7 @@ class find_163_img:
         self.images = dict(zip(self.label, self.img_addr))
 
 # TODO 增加下载cl的类
-def download_imgs(download_dir,img_addr,img_label,classified):
+def download_imgs(download_dir,img_addr,classified):
     # make_index=True
     classified_PATH=''.join((download_dir,classified,'/'))
     To_be_down=len(img_addr)
@@ -39,12 +54,12 @@ def download_imgs(download_dir,img_addr,img_label,classified):
             print('Downloading Abort.')
             exit(0)
 
-    for image_index in range(len(img_addr)):
+    for image in img_addr:
         PATH = ''.join((classified_PATH, \
-                        img_label[image_index], '_', \
-                        os.path.basename(img_addr[image_index])))
+                        str(img_addr.index('image') + 1), '_', \
+                        os.path.basename(image)))
         r=requests.get(img_addr[image_index], stream=True)
-        if r.status_code==200:
+        if r.status_code == 200:
             with open(PATH, 'wb') as f:
                 for chunk in r.iter_content():
                     f.write(chunk)
@@ -61,4 +76,4 @@ except:
     url = input('Album Address: ')
 img_163 = find_163_img(url)
 download_dir='d:/163/'
-download_imgs(download_dir,img_163.img_addr,img_163.label,img_163.article_name)
+download_imgs(download_dir,img_163.img_addr,img_163.article_name)
