fa036248e73efdbee794d0896ce71b900f0568f5
woohaha <realwoohaha@gmail.com>
2014-04-16 13:22:02 +0800

    加入註釋

--- dl.py
+++ dl.py
@@ -12,10 +12,20 @@ import json
 
 
 # initial
-failed = set()
-global header
+failed = set() # 聲明下載下載失敗的圖片集合，用集合可以避免重複
+global header # 聲明全局請求頭
 header = {'User-Agent':
           'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0'}
+
+class find_example_img:
+    """
+    這是範例。
+    """
+
+    def __init(self, url):
+        self.url = url # 傳入的url
+        self.article_name = '這個attr用於劃分子目錄'
+        self.img_addr = ['這個attr是一個list','用於保存圖片地址']
 
 
 class find_cl_img:
@@ -177,15 +187,15 @@ class find_curator_img:
 
 
 def MT_download(download_dir, img_addrs, classified, workers=10):
-    def download(img_addr, img_index):
+    def download(img_addr, img_index):# 真正工作的下載函數
 
         PATH = ''.join((classified_PATH,
                         str(img_index + 1).zfill(2), '_',
-                        os.path.basename(img_addr)))
+                        os.path.basename(img_addr))) # 構造保存地址
         try:
             img_status = os.stat(PATH).st_size
         except FileNotFoundError:
-            img_status = 0
+            img_status = 0 # 取得已下載文件的大小,大小與請求頭一致則不下載
         try:
             r = requests.get(img_addr, headers=header, stream=True)
             if r.ok and img_status != int(r.headers['content-length']):
@@ -204,7 +214,7 @@ def MT_download(download_dir, img_addrs, classified, workers=10):
                       .format(len(os.listdir(classified_PATH))))
         if go_on.lower() == 'n':
             print('Downloading Abort.')
-            exit(0)
+            exit(0) # 構建分類目錄
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
         futureIteams = {executor.submit(
@@ -217,7 +227,7 @@ def MT_download(download_dir, img_addrs, classified, workers=10):
                     failed.remove(url)
             except Exception as exc:
                 print('{} generated an exception: {}'.format(url, exc))
-                failed.add(url)
+                failed.add(url) # 多線程執行下載函數
 
     if failed:
         prompt = input(
@@ -282,34 +292,35 @@ if __name__ == '__main__':
     try:
         url = sys.argv[1]
     except:
-        url = input('Album Address: ')
+        url = input('Album Address: ') # 若參數無地址傳入，則要求填寫
 
-# TODO add record library
-    MT = True
-    workers = 10
+    MT = True # 默認開啓多線程下載
+    workers = 10 # 線程數默認為10
+    coll_dir = '/coll'
     if 'cl' in url.split('/')[2]:
         img = find_cl_img(url)
         download_dir = os.path.expanduser('~') + '/lll/'
     elif '163' in url.split('/')[2]:
         img = find_163_img(url)
-        download_dir = os.path.expanduser('~') + '/163/'
+        download_dir = os.path.expanduser('~') + coll_dir + '/163/'
     elif 'meizitu' in url.split('/')[2]:
         img = find_meizitu_img(url)
-        download_dir = os.path.expanduser('~') + '/meizitu/'
+        download_dir = os.path.expanduser('~') + coll_dir + '/meizitu/'
     elif 'curator' in url.split('/')[2]:
         img = find_curator_img(url)
-        download_dir = os.path.expanduser('~') + '/curator/'
+        download_dir = os.path.expanduser('~') + coll_dir + '/curator/'
     elif 'flickr' in url.split('/')[2]:
         img = find_flickr_img(url)
-        download_dir = os.path.expanduser('~') + '/flickr/'
+        download_dir = os.path.expanduser('~') + coll_dir + '/flickr/'
     elif 'poco' in url.split('/')[2]:
         img = find_poco_img(url)
-        download_dir = os.path.expanduser('~') + '/poco/'
+        download_dir = os.path.expanduser('~') + coll_dir + '/poco/'
         # MT=False
-        workers = 2
+        workers = 2 # poco會限制同時下載數，所以減少下載線程
     elif 'instagram' in url.split('/')[2]:
+# TODO instagram 有部分圖片使用fackbook的cdn，要考慮加入proxy功能
         img = find_instagram_img(url)
-        download_dir = os.path.expanduser('~') + '/instagram/'
+        download_dir = os.path.expanduser('~') + coll_dir + '/instagram/'
         with open(download_dir+img.article_name + '.dl', 'w')as f:
             f.write('\n'.join(img.img_addr))
         print('Total {}'.format(len(img.img_addr)))
@@ -318,8 +329,7 @@ if __name__ == '__main__':
             sys.exit(0)
 
     MT_download(download_dir, img.img_addr, img.article_name, workers=workers) if MT else download_queue(
-        download_dir, img.img_addr, img.article_name)
-    mkindex(download_dir, img.article_name)
+        download_dir, img.img_addr, img.article_name) #下載
+    mkindex(download_dir, img.article_name) # 寫一個index.html
 
-# download_queue(download_dir, img.img_addr, img.article_name)
 #print(download_dir+'\n'+ str(len(img.img_addr))+'\n'+ img.article_name)
